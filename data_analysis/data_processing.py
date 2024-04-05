7f5ace635169cb3d674e4a1990235c161ede4074
izabela <iza.kalenik@gmail.com>
2024-04-06 00:27:06 +0200

    loaded a dataset

---
+++ data_analysis/data_processing.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from IPython.core.display_functions import display
+
+
+def load_dataset():
+    file = '../dataset/suicide_rates_socioeconomic_factors.csv'
+    data = pd.read_csv(file,
+                       usecols=['RegionName', 'CountryName', 'Year', 'Sex',
+                                'SuicideCount',
+                                'Population', 'GDPPerCapita', 'EmploymentPopulationRatio'])
+
+    data = data.dropna()  # Drop rows with null or NaN values
+    data = data.reset_index(drop=True)
+    data_filtered = data[(data['Year'] >= 2010) & (data['Year'] <= 2022)]
+
+    df = pd.DataFrame({'Year': data_filtered['Year'],
+                       'RegionName': data_filtered['RegionName'],
+                       'CountryName': data_filtered['CountryName'],
+                       'Sex': data_filtered['Sex'],
+                       'SuicideCount': data_filtered['SuicideCount'],
+                       'Population': data_filtered['Population'],
+                       'GDPPerCapita': data_filtered['GDPPerCapita'],
+                       'EmploymentPopulationRatio': data_filtered['EmploymentPopulationRatio']})
+
+    df = df.sort_values(by='Year')
+    df = df.reset_index(drop=True)
+
+    display(df)
+
+    return data
---
+++ data_analysis/main.py
@@ -0,0 +1,9 @@
+from data_analysis.data_processing import load_dataset
+
+
+def run():
+    load_dataset()
+
+
+if __name__ == "__main__":
+    run()
